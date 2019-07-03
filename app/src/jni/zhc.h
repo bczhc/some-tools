a832a80ecfe36f197c4be6c236aea9095baf6029
bczhc <bczhc0@126.com>
2019-07-04 00:35:18 +0800

    一把梭 未测试

--- app/src/jni/zhc.h
+++ app/src/jni/zhc.h
@@ -11,42 +11,19 @@
 #include <string.h>
 #include <ctype.h>
 
-#pragma clang diagnostic push
-#pragma ide diagnostic ignored "OCUnusedGlobalDeclarationInspection"
 #ifndef ARR_len
 #define ARR_len(x) (sizeof(x) / sizeof(x)[0])
 #endif
 #define dl long long
 #define usi unsigned int
 
-char *ToUpperCase(char *Dest, const char *string);
-
 void PrintArr(const char arr[], int len);
-
-dl m_pow(dl base, dl exponent);
-
-int BinToDec(const char *NumStr);
-
-void printArr(const char *a, int length);
-
-char *substring(char *Dest, const char *source, int beginIndex, int endIndex);
-
-void substr(char **Dest, const char *source, int from, int length);
-
-char *substr2(char *Dest, const char *source, int from, int length);
 
 
 long long getFileSize(FILE *fp);
 
-int getIntegerLen(int x);
-
-void Scanf(char **Dest);
-
-void strcpyAndCat_auto(char **Dest, const char *cpy_s, const char *cat_s);
-
 void strcat_auto(char **sourceDest, const char *cat_s);
 
-void charToCharPtr(char **Dest, char c);
 /**
  *
  * @param string s
@@ -57,72 +34,6 @@ void charToCharPtr(char **Dest, char c);
  * int *p = NULL;
  * int t = this(&p, "a1b1c1", "1"); => p[0] = 1, p[1] = 3, p[2] = 5; t = 3;
  */
-usi strInStrCount(int **Dest, const char *string, const char *s);
-
-/**
- * String split
- * @param Dest Dest
- * @param str String
- * @param splitChar as separation
- * @use
- * void ***r = NULL;
- * split(&r, str1, str2);
- * int i = *((int *) (r[0][0]));// element count
- * char **R = ((char **) ((char ***) r)[1]); //char * result
- * for (int j = 0; j < i; ++j) {
-        printf("%s\n", R[j]);
-    }
- *//*
-void split(void ****Dest, char *str, const char *splitChar) {
-    *Dest = (void ***) malloc((size_t) (sizeof(char **) * 2));
-    ((*Dest)[0]) = (void **) malloc((size_t) (sizeof(int *) * 1));
-    (*Dest)[0][0] = (void *) malloc((size_t) (size_t) (sizeof(int)));
-    (*Dest)[1] = (void **) malloc((size_t) (sizeof(void *) * 3));
-    char *r = NULL;
-    usi sS = strlen(str) + 1, splitChar_s = strlen(splitChar) + 1;
-    char str_charArr[sS], splitChar_charArr[splitChar_s];
-    for (int j = 0; j < sS - 1; ++j) {
-        str_charArr[j] = str[j];
-    }
-    str_charArr[sS - 1] = 0x0;
-    for (int j = 0; j < splitChar_s - 1; ++j) {
-        splitChar_charArr[j] = splitChar[j];
-    }
-    splitChar_charArr[splitChar_s - 1] = 0x0;
-    usi eC = strInStrCount(str, splitChar) + 1;
-    if (str[0] == splitChar[0]) eC--;
-    if (str[sS - 2] == splitChar[splitChar_s - 2]) eC--;
-    if (eC != 1) {
-        goto n;
-    }
-    *((int *) ((*Dest)[0][0])) = (int) 0;
-    strcpy((*Dest)[1][0], "");
-    return;
-    n:
-    *((int *) ((*Dest)[0][0])) = (int) eC;
-    (*Dest)[1] = (void **) malloc((size_t) (sizeof(char *) * eC));
-    r = strtok(str_charArr, splitChar_charArr);
-    int a_i = 0;
-    while (r != NULL) {
-        int rS = strlen(r) + 1;
-        (*Dest)[1][a_i] = ((char *) malloc((size_t) rS));
-        strcpy((*Dest)[1][a_i], r);
-        r = strtok(NULL, splitChar_charArr);
-        ++a_i;
-    }
-}*/
-
-int Str_Cmp_nMatchCase(const char *a, const char *b);
-
-void m_itoa(char **Dest, int i);
-
-/*void m_lltoa(char **Dest, const dl int ll) {
-
-}*/
-
-int split(char ***Dest, const char *SourceString, const char *SplitStr);
-
-int cmpIntArray(const int *a1, int a1Len, const int *a2, int a2Len);
 
 int cmpCharArray(const char *a1, int a1Len, const char *a2, int a2Len);
 
