8fe651aa28e8c5cca3c596e3d3d2cb9b34a836da
bczhc <bczhc0@126.com>
2019-08-01 08:45:14 +0800

    正常传递intent和解析json 加入MainActivity构参使其最优先执行

---
+++ app/src/jni/cmake-build-debug/CMakeFiles/qmc_test.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/root/myHome/zhc/code/code/Android/QMCFLAC/app/src/jni/qmcLib.c" "/root/myHome/zhc/code/code/Android/QMCFLAC/app/src/jni/cmake-build-debug/CMakeFiles/qmc_test.dir/qmcLib.c.o"
+  "/root/myHome/zhc/code/code/Android/QMCFLAC/app/src/jni/test/qmc_test.c" "/root/myHome/zhc/code/code/Android/QMCFLAC/app/src/jni/cmake-build-debug/CMakeFiles/qmc_test.dir/test/qmc_test.c.o"
+  "/root/myHome/zhc/code/code/Android/QMCFLAC/app/src/jni/zhc.c" "/root/myHome/zhc/code/code/Android/QMCFLAC/app/src/jni/cmake-build-debug/CMakeFiles/qmc_test.dir/zhc.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ app/src/jni/cmake-build-debug/CMakeFiles/qmc_test.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/qmc_test.dir/test/qmc_test.c.o"
+  "CMakeFiles/qmc_test.dir/zhc.c.o"
+  "CMakeFiles/qmc_test.dir/qmcLib.c.o"
+  "qmc_test.pdb"
+  "qmc_test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/qmc_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
