5110a7e7e20d4b424a3ec54692ffa56d2ab08f20
bczhc <bczhc0@126.com>
2019-08-03 12:49:12 +0800

    更新计算tools，有计算Pi 马上小吐司 前面bug先放一放

--- app/src/obj/local/arm64-v8a/objs/doJNI/qmcLib.o.d
+++
@@ -1,8 +0,0 @@
-/home/zhc/code/code/Android/QMCFLAC/app/src/obj/local/arm64-v8a/objs/doJNI/qmcLib.o: \
-  /home/zhc/code/code/Android/QMCFLAC/app/src/jni/qmcLib.c \
-  /home/zhc/code/code/Android/QMCFLAC/app/src/jni/qmcLib.h \
-  /home/zhc/code/code/Android/QMCFLAC/app/src/jni/zhc.h
-
-/home/zhc/code/code/Android/QMCFLAC/app/src/jni/qmcLib.h:
-
-/home/zhc/code/code/Android/QMCFLAC/app/src/jni/zhc.h:
--- app/src/obj/local/x86_64/objs/doJNI/qmcLib.o.d
+++
@@ -1,8 +0,0 @@
-/home/zhc/code/code/Android/QMCFLAC/app/src/obj/local/x86_64/objs/doJNI/qmcLib.o: \
-  /home/zhc/code/code/Android/QMCFLAC/app/src/jni/qmcLib.c \
-  /home/zhc/code/code/Android/QMCFLAC/app/src/jni/qmcLib.h \
-  /home/zhc/code/code/Android/QMCFLAC/app/src/jni/zhc.h
-
-/home/zhc/code/code/Android/QMCFLAC/app/src/jni/qmcLib.h:
-
-/home/zhc/code/code/Android/QMCFLAC/app/src/jni/zhc.h:
