2e8af4bb653a28b8bb6a416a30e8fd80b57c84f7
bczhc <bczhc0@126.com>
2019-08-04 11:28:21 +0800

    分开数组：下拉栏数据数组、json第一个分支数据数组、设置要显示用到的数组 放刚才的数组到array.xml，读取在其它线程中，超多CountDownLatch。。 继承BaseActivity（重写了配置改变（屏幕旋转等）的动作），但其实没有它也是可以保持屏幕旋转后不重新执行onCreat方法的。 改xml名字

---
+++ app/src/main/java/com/zhc/utils/BaseActivity.java
@@ -0,0 +1,45 @@
+package com.zhc.utils;
+
+import android.content.res.Configuration;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+public class BaseActivity extends AppCompatActivity {
+
+    /**
+     * onConfigurationChanged
+     * the package:android.content.res.Configuration.
+     *
+     * @param newConfig, The new device configuration.
+     *                   当设备配置信息有改动（比如屏幕方向的改变，实体键盘的推开或合上等）时，
+     *                   并且如果此时有activity正在运行，系统会调用这个函数。
+     *                   注意：onConfigurationChanged只会监测应用程序在AndroidManifest.xml中通过
+     *                   android:configChanges="xxxx"指定的配置类型的改动；
+     *                   而对于其他配置的更改，则系统会onDestroy()当前Activity，然后重启一个新的Activity实例。
+     */
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        String TAG = "onConfigurationChanged";
+        // 检测屏幕的方向：纵向或横向
+        if (this.getResources().getConfiguration().orientation
+                == Configuration.ORIENTATION_LANDSCAPE) {
+            //当前为横屏， 在此处添加额外的处理代码
+            Log.d(TAG, "onConfigurationChanged: 当前为横屏");
+        } else if (this.getResources().getConfiguration().orientation
+                == Configuration.ORIENTATION_PORTRAIT) {
+            //当前为竖屏， 在此处添加额外的处理代码
+            Log.d(TAG, "onConfigurationChanged: 当前为竖屏");
+        }
+        //检测实体键盘的状态：推出或者合上
+        if (newConfig.hardKeyboardHidden
+                == Configuration.HARDKEYBOARDHIDDEN_NO) {
+            //实体键盘处于推出状态，在此处添加额外的处理代码
+            Log.d(TAG, "onConfigurationChanged: 实体键盘处于推出状态");
+        } else if (newConfig.hardKeyboardHidden
+                == Configuration.HARDKEYBOARDHIDDEN_YES) {
+            //实体键盘处于合上状态，在此处添加额外的处理代码
+            Log.d(TAG, "onConfigurationChanged: 实体键盘处于合上状态");
+        }
+    }
+}
