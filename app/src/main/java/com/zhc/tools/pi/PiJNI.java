a22ffed58789fa7eb52ae26ddebb575de817672d
bczhc <bczhc0@126.com>
2019-08-04 14:10:58 +0800

    Pi捕捉int，放入StringBuilder方法。

--- app/src/main/java/com/zhc/tools/pi/PiJNI.java
+++ app/src/main/java/com/zhc/tools/pi/PiJNI.java
@@ -4,15 +4,6 @@ package com.zhc.tools.pi;
 import android.widget.EditText;
 
 class PiJNI {
-    static int[] ints;
-
-    PiJNI(int bN) {
-        ints = new int[bN / 4 + 1];
-    }
-
-    PiJNI() {
-    }
-
     StringBuilder sb;
     EditText o = null;
 
@@ -29,7 +20,7 @@ class PiJNI {
     native void gen(int bN);
 
     @SuppressWarnings("unused")
-    public void O(int a, int i) {
+    public void O(int a) {
 //        CountDownLatch latch = new CountDownLatch(1);
         /*runOnUiThread(() -> {
             o.setText(String.format("%s%s", s, o.getText().toString()));
@@ -42,18 +33,6 @@ class PiJNI {
             e.printStackTrace();
         }*/
 //        this.sb.append(s);
-        new T(i).f(a);
-    }
-
-    static class T {
-        private int i;
-
-        T(int index) {
-            this.i = index;
-        }
-
-        void f(int a) {
-            ints[this.i] = a;
-        }
+        this.sb.append(a);
     }
 }
