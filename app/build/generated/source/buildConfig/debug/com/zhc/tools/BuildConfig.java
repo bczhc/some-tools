e433b07c9acf74aca5255431e4d24ef3fc721ff0
bczhc <bczhc0@126.com>
2019-08-16 00:37:37 +0800

    悬浮画板branch

---
+++ app/build/generated/source/buildConfig/debug/com/zhc/tools/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.zhc.tools;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.zhc.tools";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 14;
+  public static final String VERSION_NAME = "3.1.0.0_20190813🐸";
+}
