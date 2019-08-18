dc7afcdfd0781d3dbd7ec992973660c59c3aaa95
bczhc <bczhc0@126.com>
2019-08-18 11:19:41 +0800

    修复 jni。。。。。 以后要养成 注释/s:size; /ss:string size(size + 1('\0')) 防止杀死 switch的时候就操作 傅里叶级数函数计算，固定线程排 函数生成器 放大缩小+鬼畜移动

--- app/src/main/java/com/zhc/tools/functiondrawing/FunctionDrawingView.java
+++ app/src/main/java/com/zhc/tools/functiondrawing/FunctionDrawingView.java
@@ -5,14 +5,18 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.view.MotionEvent;
 import android.view.View;
+import com.zhc.u.MathFloatFunctionInterface;
 
 @SuppressLint("ViewConstructor")
 class FunctionDrawingView extends View {
     private int width, height;
     private Paint mPaint;
-    private boolean firstDraw = true;
-    private FunctionInterface f;
+    private MathFloatFunctionInterface f;
+    private float xLength = 30, yLength = 30;
+    private float phaseC = 0;
+    private float yC = 0;
 
     FunctionDrawingView(Context context, int width, int height) {
         super(context);
@@ -32,22 +36,54 @@ class FunctionDrawingView extends View {
         canvas.drawLine(((float) (width / 2)), 0F, ((float) (width / 2) - 15F), 15F, mPaint);
         canvas.drawLine(((float) (width / 2)), 0F, ((float) (width / 2) + 15F), 15F, mPaint);
         for (int i = 0; i < width; i++) {
-            float x = i * 200F / (float) width -100F;
-            float y = this.f.f(x);
-            float drawY = -(y * (float) width / 200 - (float) height / 2);
+            float x = i * 2 * xLength / (float) width - xLength;
+            float y = this.f.f(x + phaseC) + yC;
+            float drawY = -(y * (float) width / yLength - (float) height / 2);
             canvas.drawPoint(i, drawY, mPaint);
         }
     }
 
-    void drawFunction(FunctionInterface f, int strokeWidth) {
-        mPaint.setStrokeWidth(strokeWidth);
+    void zoom(float newXLength, float newYLength) {
+        this.xLength = newXLength;
+        this.yLength = newYLength;
+        invalidate();
+    }
+
+    void drawFunction(MathFloatFunctionInterface f) {
+
+        mPaint.setStrokeWidth(4);
         mPaint.setStrokeJoin(Paint.Join.ROUND);//使画笔更加圆润
         mPaint.setStrokeCap(Paint.Cap.ROUND);//同上
         this.f = f;
         invalidate();
     }
 
-    interface FunctionInterface {
-        float f(float x);
+    private float lastX, lastY;
+    private float nowX = 0, nowY = 0;
+    private float pX, pY;
+
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                lastX = event.getRawX();
+                lastY = event.getRawY();
+                pX = nowX;
+                pY = nowY;
+                break;
+            case MotionEvent.ACTION_MOVE:
+                float dX = event.getRawX() - lastX;
+                float dY = event.getRawY() - lastY;
+                nowX = pX + dX;
+                nowY = pY + dY;
+                this.phaseC = -nowX * xLength / width;
+                this.yC = -nowY * yLength / height;
+                invalidate();
+            case MotionEvent.ACTION_UP:
+                this.performClick();
+                break;
+        }
+        return true;
     }
 }
