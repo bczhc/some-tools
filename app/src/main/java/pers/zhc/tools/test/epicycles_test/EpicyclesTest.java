92f564b6fdb0df170ae763a1918a3cd84ecf018a
bczhc <bczhc0@126.com>
2019-09-21 23:24:21 +0800

    本轮绘制基本功能实现

--- app/src/main/java/pers/zhc/tools/test/epicycles_test/EpicyclesTest.java
+++ app/src/main/java/pers/zhc/tools/test/epicycles_test/EpicyclesTest.java
@@ -3,6 +3,7 @@ package pers.zhc.tools.test.epicycles_test;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import pers.zhc.u.Random;
 import pers.zhc.u.math.fourier.EpicyclesSequence;
 import pers.zhc.u.math.util.ComplexValue;
 
@@ -11,15 +12,41 @@ public class EpicyclesTest extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EpicyclesSequence epicyclesSequence = new EpicyclesSequence();
-        epicyclesSequence.put(1, new ComplexValue(30D, 30D));
-        epicyclesSequence.put(1, new ComplexValue(40D, -30D));
-        epicyclesSequence.put(-1, new ComplexValue(50D, 50D));
+        /*epicyclesSequence.put(3, new ComplexValue(30D, 30D));
+        epicyclesSequence.put(3, new ComplexValue(30D, 30D));
+        epicyclesSequence.put(3, new ComplexValue(30D, 30D));
+        epicyclesSequence.put(4, new ComplexValue(40D, -30D));
+        epicyclesSequence.put(4, new ComplexValue(40D, -30D));
+        epicyclesSequence.put(-4, new ComplexValue(50D, 50D));
+        epicyclesSequence.put(-4, new ComplexValue(50D, 50D));
+        epicyclesSequence.put(2, new ComplexValue(-20D, 60D));
         epicyclesSequence.put(2, new ComplexValue(-20D, 60D));
         epicyclesSequence.put(-2, new ComplexValue(100D, 100D));
+        epicyclesSequence.put(-2, new ComplexValue(100D, 100D));
         epicyclesSequence.put(-3, new ComplexValue(-100D, -10D));
-        epicyclesSequence.put(3, new ComplexValue(50D, -40D));
+        epicyclesSequence.put(-3, new ComplexValue(-100D, -10D));
+        epicyclesSequence.put(6, new ComplexValue(50D, -40D));
+        epicyclesSequence.put(6, new ComplexValue(50D, -40D));
+        epicyclesSequence.put(-4, new ComplexValue(100D, 20D));
         epicyclesSequence.put(-4, new ComplexValue(100D, 20D));
         epicyclesSequence.put(4, new ComplexValue(0D, -50D));
+        epicyclesSequence.put(4, new ComplexValue(0D, -50D));
+        epicyclesSequence.put(4, new ComplexValue(0D, -50D));
+        epicyclesSequence.put(4, new ComplexValue(0D, -50D));
+        epicyclesSequence.put(4, new ComplexValue(0D, -50D));
+        epicyclesSequence.put(4, new ComplexValue(0D, -50D));*/
+        for (int i = 0; i < 50; i++) {
+            epicyclesSequence.put(Random.ran_sc(-10, 10), new ComplexValue(Math.random() - Math.random() / 2, Math.random() - Math.random() / 2));
+        }
+//        epicyclesSequence.put(4, 1, 1);
+//        epicyclesSequence.put(1, new ComplexValue(100, 100));
+
+
+        /*epicyclesSequence.put(0, 10.72, 16.52);
+        epicyclesSequence.put(1, -12.64, 20.90);
+        epicyclesSequence.put(-1, -135.66, -45.57);
+        epicyclesSequence.put(2, -44.85, -23.71);
+        epicyclesSequence.put(-2, 66.75, -53.07);*/
         EpicyclesView epicyclesView = new EpicyclesView(this, epicyclesSequence);
         setContentView(epicyclesView);
     }
