b0e15872dda1263c4e5acb3bf70bb11bf0428631
bczhc <bczhc0@126.com>
2019-12-29 07:42:08 +0800

    支持包换android-support-v4 28.0.0

--- app/build/generated/source/buildConfig/androidTest/debug/pers/zhc/tools/test/BuildConfig.java
+++ app/build/generated/source/buildConfig/androidTest/debug/pers/zhc/tools/test/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "pers.zhc.tools.test";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 28;
+  public static final int VERSION_CODE = 29;
   public static final String VERSION_NAME = "4.6.0.1_20191222🐸_";
 }
--- app/src/main/java/pers/zhc/tools/toast/AToast.java
+++ app/src/main/java/pers/zhc/tools/toast/AToast.java
@@ -2,14 +2,14 @@ package pers.zhc.tools.toast;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import pers.zhc.tools.BaseActivity;
 import pers.zhc.tools.R;
 
-public class AToast extends AppCompatActivity {
+public class AToast extends BaseActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
