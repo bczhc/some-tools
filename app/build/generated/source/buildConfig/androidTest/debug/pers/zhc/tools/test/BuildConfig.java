7379c8ab93185434e6f19e73e225dc63638d52dc
bczhc <bczhc0@126.com>
2019-12-28 16:53:22 +0800

    双指移动+缩放手势view demo

---
+++ app/build/generated/source/buildConfig/androidTest/debug/pers/zhc/tools/test/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package pers.zhc.tools.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "pers.zhc.tools.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 28;
+  public static final String VERSION_NAME = "4.6.0.1_20191222🐸_";
+}
