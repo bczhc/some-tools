bba254c48a74d880fbfb119b1a84a93612b70939
bczhc <bczhc0@126.com>
2020-01-01 08:48:02 +0800

    add .gitignore file modify app icon

---
+++ app/src/jni/c/cmake-build-debug/CMakeFiles/verification.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/verification.dir/verification/verify.c.o"
+  "CMakeFiles/verification.dir/home/zhc/code/code/Android/QMCFLAC/app/src/jni/zhc.c.o"
+  "CMakeFiles/verification.dir/codecs/qmcLib.c.o"
+  "CMakeFiles/verification.dir/floatingboard/fb_tools.c.o"
+  "verification.pdb"
+  "verification"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/verification.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
