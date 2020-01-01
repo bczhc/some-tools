9c43da58a312e8aea66d8bed2f71c682f0c41b72
bczhc <bczhc0@126.com>
2020-01-01 08:48:02 +0800

    add .gitignore file modify app icon

---
+++ app/src/jni/c/cmake-build-debug/CMakeFiles/doJni.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/doJni.dir/codecs/doJNI.c.o"
+  "doJni.pdb"
+  "doJni"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/doJni.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
