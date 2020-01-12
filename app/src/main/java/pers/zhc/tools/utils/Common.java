b01583f1131beaf3bf20e97d5a8f497550b15ecc
bczhc <bczhc0@126.com>
2020-01-12 15:05:01 +0800

    fix issue #6 on github

--- app/src/main/java/pers/zhc/tools/utils/Common.java
+++ app/src/main/java/pers/zhc/tools/utils/Common.java
@@ -1,11 +1,24 @@
 package pers.zhc.tools.utils;
 
 import android.app.Activity;
+import android.content.Context;
+import android.os.Build;
+import android.os.Environment;
 import android.widget.Toast;
+
+import java.util.Objects;
 
 public class Common {
     public static void showException(Exception e, Activity activity) {
         e.printStackTrace();
         activity.runOnUiThread(() -> Toast.makeText(activity, e.toString(), Toast.LENGTH_SHORT).show());
+    }
+
+    public static String getExternalStoragePath(Context ctx) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+            //noinspection deprecation
+            return Environment.getExternalStorageDirectory().toString();
+        }
+        return Objects.requireNonNull(ctx.getExternalFilesDir(null)).toString();
     }
 }
