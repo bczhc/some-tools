667ac6bdb5f2e42ed62c16806743fac15ba75778
bczhc <bczhc0@126.com>
2020-01-26 12:37:23 +0800

    perf: 悬浮画板保存路径，减少new

--- app/src/jni/c/floatingboard/fb_tools.c
+++ app/src/jni/c/floatingboard/fb_tools.c
@@ -7,32 +7,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-JNIEXPORT jbyteArray JNICALL Java_pers_zhc_tools_floatingdrawing_JNI_floatToByteArray
-        (JNIEnv *env, jobject obj, jfloat a) {
+JNIEXPORT void  JNICALL Java_pers_zhc_tools_floatingdrawing_JNI_floatToByteArray
+        (JNIEnv *env, jobject obj, jbyteArray dst, jfloat a) {
     JNIEnv e = *env;
     float f = (float) a;
     char *c = (char *) &f;
-    jbyteArray r = e->NewByteArray(env, 4);
     jbyte R[4];
-    for (int i = 0; i < 4; ++i) {
-        R[i] = c[i];
-    }
-    e->SetByteArrayRegion(env, r, 0, 4, R);
-    return (jbyteArray) r;
+    R[0] = c[0];
+    R[1] = c[1];
+    R[2] = c[2];
+    R[3] = c[3];
+    e->SetByteArrayRegion(env, dst, 0, 4, R);
 }
 
-JNIEXPORT jbyteArray JNICALL Java_pers_zhc_tools_floatingdrawing_JNI_intToByteArray
-        (JNIEnv *env, jobject obj, jint j) {
+JNIEXPORT void  JNICALL Java_pers_zhc_tools_floatingdrawing_JNI_intToByteArray
+        (JNIEnv *env, jobject obj, jbyteArray dst, jint j) {
     JNIEnv e = *env;
     int i = (int) j;
     char *c = (char *) &i;
-    jbyteArray r = e->NewByteArray(env, 4);
     jbyte R[4];
-    for (int k = 0; k < 4; ++k) {
-        R[k] = c[k];
-    }
-    e->SetByteArrayRegion(env, r, 0, 4, R);
-    return (jbyteArray) r;
+    R[0] = c[0];
+    R[1] = c[1];
+    R[2] = c[2];
+    R[3] = c[3];
+    e->SetByteArrayRegion(env, dst, 0, 4, R);
 }
 
 JNIEXPORT jfloat JNICALL Java_pers_zhc_tools_floatingdrawing_JNI_byteArrayToFloat
