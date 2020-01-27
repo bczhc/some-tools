b95cb91888335f47370c0790c80fa74723a8824c
bczhc <bczhc0@126.com>
2020-01-27 20:11:15 +0800

    fix typo, perf and fix issue #5, #7, #8 on github

--- app/src/main/java/pers/zhc/tools/utils/Common.java
+++ app/src/main/java/pers/zhc/tools/utils/Common.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.content.Context;
 import android.os.Build;
 import android.os.Environment;
-import pers.zhc.tools.BuildConfig;
 
 import java.util.Objects;
 
