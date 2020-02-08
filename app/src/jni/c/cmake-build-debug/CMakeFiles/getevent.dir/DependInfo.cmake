b577df3662ca94aa0d2fbed394d6b9198f61887a
bczhc <bczhc0@126.com>
2020-02-08 18:53:46 +0800

    issue #54, #58. path 2.1!

--- app/src/jni/c/cmake-build-debug/CMakeFiles/getevent.dir/DependInfo.cmake
+++ app/src/jni/c/cmake-build-debug/CMakeFiles/getevent.dir/DependInfo.cmake
@@ -4,13 +4,13 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "/home/zhc/code/code/Android/some-tools/app/src/jni/c/codecs/doJNI.c" "/home/zhc/code/code/Android/some-tools/app/src/jni/c/cmake-build-debug/CMakeFiles/doJni.dir/codecs/doJNI.c.o"
+  "/home/zhc/code/code/Android/some-tools/app/src/jni/c/getevent/geteventV2.c" "/home/zhc/code/code/Android/some-tools/app/src/jni/c/cmake-build-debug/CMakeFiles/getevent.dir/getevent/geteventV2.c.o"
   )
 set(CMAKE_C_COMPILER_ID "GNU")
 
 # The include file search paths:
 set(CMAKE_C_TARGET_INCLUDE_PATH
-  "/home/zhc/bin/android-sdk-linux/ndk-bundle/sysroot/usr/include"
+  "/home/zhc/bin/android-sdk-linux/ndk/21.0.6113669/sysroot/usr/include"
   )
 
 # Targets to which this target links.
