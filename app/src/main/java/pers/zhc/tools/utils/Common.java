d38f56379224a1495a4385da8d189befbaab2600
bczhc <bczhc0@126.com>
2020-02-15 23:40:03 +0800

    应用更新 #71

--- app/src/main/java/pers/zhc/tools/utils/Common.java
+++ app/src/main/java/pers/zhc/tools/utils/Common.java
@@ -2,9 +2,14 @@ package pers.zhc.tools.utils;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Environment;
+import android.support.v4.content.FileProvider;
+import pers.zhc.tools.BuildConfig;
 
+import java.io.File;
 import java.util.Objects;
 
 public class Common {
@@ -19,5 +24,25 @@ public class Common {
             return Environment.getExternalStorageDirectory().toString();
         }
         return Objects.requireNonNull(ctx.getExternalFilesDir(null)).toString();
+    }
+
+    /**
+     * 安装apk
+     *
+     * @param context  ctx
+     * @param apk 本地apk
+     */
+    public static void installApk(Context context, File apk) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            Uri contentUri = FileProvider.getUriForFile(context, BuildConfig.APPLICATION_ID + ".fileprovider", apk);
+            intent.setDataAndType(contentUri, "application/vnd.android.package-archive");
+        } else {
+            Uri uri = Uri.fromFile(apk);
+            intent.setDataAndType(uri, "application/vnd.android.package-archive");
+        }
+        context.startActivity(intent);
     }
 }
