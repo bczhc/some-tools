a467c385fe640ec76f446c4e8fccf79b4aa76816
bczhc <bczhc0@126.com>
2020-02-22 11:22:37 +0800

    悬浮画板：取色截屏卡顿的问题

--- app/src/main/java/pers/zhc/tools/test/service/MyService.java
+++ app/src/main/java/pers/zhc/tools/test/service/MyService.java
@@ -4,24 +4,25 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import pers.zhc.tools.utils.ToastUtils;
 
 public class MyService extends Service {
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        System.out.println("onBind");
+        ToastUtils.show(this, "onBind");
         return null;
     }
 
     @Override
     public void onCreate() {
-        System.out.println("onCreate");
+        ToastUtils.show(this, "onCreate");
         super.onCreate();
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        System.out.println("onStartCommand");
+        ToastUtils.show(this, "onStartCommand");
         return super.onStartCommand(intent, flags, startId);
     }
 
