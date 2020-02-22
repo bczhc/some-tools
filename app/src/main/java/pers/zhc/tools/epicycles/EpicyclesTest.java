862729a421224c38f1047cac6ae1d3c304a9c234
bczhc <bczhc0@126.com>
2020-02-22 17:05:59 +0800

    稍微改吧改吧；本轮绘制回到commit b4de4f88cfc983935304ab7300408543161daa6d 但是添加了手势；

--- app/src/main/java/pers/zhc/tools/epicycles/EpicyclesTest.java
+++ app/src/main/java/pers/zhc/tools/epicycles/EpicyclesTest.java
@@ -6,10 +6,18 @@ import pers.zhc.tools.BaseActivity;
 
 public class EpicyclesTest extends BaseActivity {
 
+    private EpicyclesView epicyclesView;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EpicyclesView epicyclesView = new EpicyclesView(this, EpicyclesEdit.epicyclesSequence);
+        epicyclesView = new EpicyclesView(this, EpicyclesEdit.epicyclesSequence);
         setContentView(epicyclesView);
+    }
+
+    @Override
+    protected void onDestroy() {
+        epicyclesView.shutdownES();
+        super.onDestroy();
     }
 }
