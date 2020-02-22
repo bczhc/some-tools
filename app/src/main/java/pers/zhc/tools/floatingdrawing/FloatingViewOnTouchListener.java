8df38711fd7457da9dc345a85d21b9bd72685b12
bczhc <bczhc0@126.com>
2020-02-22 17:05:59 +0800

    稍微改吧改吧；本轮绘制回到commit b4de4f88cfc983935304ab7300408543161daa6d 但是添加了手势；

---
+++ app/src/main/java/pers/zhc/tools/floatingdrawing/FloatingViewOnTouchListener.java
@@ -0,0 +1,72 @@
+package pers.zhc.tools.floatingdrawing;
+
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+
+public class FloatingViewOnTouchListener implements View.OnTouchListener {
+    final private WindowManager.LayoutParams layoutParams;
+    private final int width;
+    private final int height;
+    private final WindowManager wm;
+    private final View view;
+    private final ViewSpec viewSpec;
+    private int lastRawX, lastRawY, paramX, paramY;
+
+    public FloatingViewOnTouchListener(WindowManager.LayoutParams WM_layoutParams, WindowManager windowManager
+            , View view, int width, int height, ViewSpec viewSpec) {
+        this.layoutParams = WM_layoutParams;
+        this.wm = windowManager;
+        this.width = width;
+        this.height = height;
+        this.view = view;
+        this.viewSpec = viewSpec;
+    }
+
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        float rawX = event.getRawX();
+        float rawY = event.getRawY();
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                lastRawX = (int) rawX;
+                lastRawY = (int) rawY;
+                paramX = layoutParams.x;
+                paramY = layoutParams.y;
+                break;
+            case MotionEvent.ACTION_MOVE:
+                int dx = (int) rawX - lastRawX;
+                int dy = (int) rawY - lastRawY;
+                int lp2_x = paramX + dx;
+                int lp2_y = paramY + dy;
+                int constrainX, constrainY;
+                constrainX = lp2_x - HSVAColorPickerRL.limitValue(lp2_x, ((int) (-width / 2F + viewSpec.width / 2F)), ((int) (width / 2F - viewSpec.width / 2F)));
+                constrainY = lp2_y - HSVAColorPickerRL.limitValue(lp2_y, ((int) (-height / 2F + viewSpec.height / 2F)), ((int) (height / 2F - viewSpec.height / 2F)));
+                if (constrainX == 0) layoutParams.x = lp2_x;
+                else paramX -= constrainX;
+                if (constrainY == 0) layoutParams.y = lp2_y;
+                else paramY -= constrainY;
+                // 更新悬浮窗位置
+                wm.updateViewLayout(view, layoutParams);
+                break;
+            case MotionEvent.ACTION_UP:
+                if (Math.abs(lastRawX - rawX) < 1 && Math.abs(lastRawY - rawY) < 1)
+                    v.performClick();
+                break;
+        }
+        return true;
+    }
+
+    public static class ViewSpec {
+        int width = 0, height = 0;
+
+        public ViewSpec(int width, int height) {
+            this.width = width;
+            this.height = height;
+        }
+
+        public ViewSpec() {
+        }
+    }
+}
---
+++ app/src/main/java/pers/zhc/tools/floatingdrawing/ScreenColorPickerView.java
@@ -0,0 +1,64 @@
+package pers.zhc.tools.floatingdrawing;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class ScreenColorPickerView extends View {
+    private int width = -1, height = -1;
+    private Paint mPaint;
+
+    public ScreenColorPickerView(Context context) {
+        super(context);
+    }
+
+    public ScreenColorPickerView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ScreenColorPickerView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawColor(Color.RED);
+    }
+
+    private void init() {
+        mPaint = new Paint();
+        mPaint.setStrokeWidth(10F);
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return true;
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int wMode = MeasureSpec.getMode(widthMeasureSpec);
+        int hMode = MeasureSpec.getMode(heightMeasureSpec);
+        boolean b = false;
+        if (width == -1 && height == -1) b = true;
+        width = ((int) (MeasureSpec.getSize(widthMeasureSpec) * .4F));
+        height = ((int) (MeasureSpec.getSize(heightMeasureSpec) * .4F));
+        int w = 0, h = 0;
+        if (wMode != MeasureSpec.AT_MOST && hMode != MeasureSpec.AT_MOST) {
+            int min = Math.min(width, height);
+            width = (height = min);
+            w = width;
+            h = height;
+        }
+        setMeasuredDimension(w, h);
+        if (b) init();
+    }
+}
