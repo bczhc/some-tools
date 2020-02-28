4f90bcb609281749121d720106378080d01f75d3
bczhc <bczhc0@126.com>
2020-02-28 23:21:00 +0800

    issue #86

---
+++ app/src/main/java/pers/zhc/tools/youdaoapi/YouDaoTranslate.java
@@ -0,0 +1,14 @@
+package pers.zhc.tools.youdaoapi;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import pers.zhc.tools.R;
+
+public class YouDaoTranslate extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.you_dao_activity);
+    }
+}
