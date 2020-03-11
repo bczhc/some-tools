6bc1ead5290934377640967cf8df4db38db0247e
bczhc <bczhc0@126.com>
2020-03-11 21:51:52 +0800

    issue #79, 弹窗是否为悬浮窗，权限检验

--- app/src/main/java/pers/zhc/tools/utils/DialogUtil.java
+++ app/src/main/java/pers/zhc/tools/utils/DialogUtil.java
@@ -1,20 +1,45 @@
 package pers.zhc.tools.utils;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.graphics.PixelFormat;
 import android.os.Build;
+import android.support.annotation.Nullable;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.EditText;
 import pers.zhc.tools.R;
+import pers.zhc.tools.floatingdrawing.FloatingDrawingBoardMainActivity;
 
 import java.util.Objects;
 
 public class DialogUtil {
-    public static void setDialogAttr(Dialog d, boolean isTransparent, int width, int height, boolean application_overlay) {
+    /**
+     *
+     * @param d d
+     * @param isTransparent b
+     * @param width w
+     * @param height h
+     * @param application_overlay overlay. null is auto
+     */
+    public static void setDialogAttr(Dialog d, boolean isTransparent, int width, int height, @Nullable Boolean application_overlay) {
+        final boolean[] overlay = {false};
+        if (application_overlay == null) {
+            new FloatingDrawingBoardMainActivity.CheckOverlayPermission(((Activity) d.getContext())) {
+                @Override
+                public void granted() {
+                    overlay[0] = true;
+                }
+
+                @Override
+                public void denied() {
+                }
+
+            };
+        } else overlay[0] = application_overlay;
         Window window;
         try {
             window = Objects.requireNonNull(d.getWindow());
@@ -22,7 +47,7 @@ public class DialogUtil {
             e.printStackTrace();
             return;
         }
-        if (application_overlay) {
+        if (overlay[0]) {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
                 window.setAttributes(new WindowManager.LayoutParams(width, height, WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY, 0, PixelFormat.RGB_888));
             } else                                 //noinspection deprecation
