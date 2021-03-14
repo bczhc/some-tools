83818f6186d1659cf212259fad16270a58292a48
bczhc <bczhc0@126.com>
2021-03-14 21:30:27 +0800

    unfinished PlaneRL

--- app/src/main/java/pers/zhc/tools/floatingdrawing/FloatingDrawingBoardService.kt
+++ app/src/main/java/pers/zhc/tools/floatingdrawing/FloatingDrawingBoardService.kt
@@ -5,10 +5,13 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.content.res.Configuration
 import android.graphics.Color
+import android.graphics.Point
 import android.os.IBinder
 import android.view.ViewGroup
 import android.view.WindowManager
+import pers.zhc.tools.utils.DisplayUtil
 
 /**
  * @author bczhc
@@ -18,7 +21,7 @@ class FloatingDrawingBoardService : Service() {
     private lateinit var wm: WindowManager
     private lateinit var pv: PaintView
     private lateinit var planeLP: WindowManager.LayoutParams
-    private lateinit var drawingBoardLP: WindowManager.LayoutParams
+    private lateinit var paintViewLP: WindowManager.LayoutParams
 
     fun registerBroadcast() {
         interactionReceiver = this.InteractionBroadcastReceiver()
@@ -31,21 +34,32 @@ class FloatingDrawingBoardService : Service() {
         return START_NOT_STICKY
     }
 
+    private fun getScreenWH(): Point {
+        val configuration = this.resources.configuration
+        return Point(DisplayUtil.dip2px(this, configuration.screenWidthDp.toFloat()),
+            DisplayUtil.dip2px(this, configuration.screenHeightDp.toFloat()))
+    }
+
     private fun init() {
         wm = this.applicationContext.getSystemService(Context.WINDOW_SERVICE) as WindowManager
 
         pv = PaintView(this)
+        // default to match the width and height of `paintViewLP`
+        pv.layoutParams =
+            ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
         pv.drawingStrokeWidth = 10F
         pv.eraserStrokeWidth = 10F
         pv.drawingColor = Color.RED
 
         planeLP = WindowManager.LayoutParams()
-        drawingBoardLP = WindowManager.LayoutParams()
+        paintViewLP = WindowManager.LayoutParams()
 
         planeLP.width = ViewGroup.LayoutParams.WRAP_CONTENT
         planeLP.height = ViewGroup.LayoutParams.WRAP_CONTENT
-        drawingBoardLP.width = ViewGroup.LayoutParams.WRAP_CONTENT
-        drawingBoardLP.height = ViewGroup.LayoutParams.WRAP_CONTENT
+
+        val screenWH = getScreenWH()
+        paintViewLP.width = screenWH.x
+        paintViewLP.height = screenWH.y
     }
 
     override fun onDestroy() {
@@ -58,18 +72,33 @@ class FloatingDrawingBoardService : Service() {
     }
 
     fun startFloatingWindow() {
-
+        wm.addView(pv, paintViewLP)
     }
 
     fun stopFloatingWindow() {
 
     }
 
+    private var lastOrientation: Int? = null
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        val screenWidth = DisplayUtil.dip2px(this, newConfig.screenHeightDp.toFloat())
+        val screenHeight = DisplayUtil.dip2px(this, newConfig.screenHeightDp.toFloat())
+        val orientation = newConfig.orientation
+
+        if (orientation != lastOrientation) {
+            // orientation changed
+            lastOrientation = orientation
+
+            paintViewLP.width = screenWidth
+            paintViewLP.height = screenHeight
+        }
+    }
+
     inner class InteractionBroadcastReceiver : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
             if (intent == null) return
-            val actionInt = intent.getIntExtra("action", 0)
-            when (actionInt) {
+            when (intent.getIntExtra("action", 0)) {
                 ACTION_START_FLOATING_BOARD -> startFloatingWindow()
                 ACTION_STOP_FLOATING_BOARD -> stopFloatingWindow()
                 else -> {
