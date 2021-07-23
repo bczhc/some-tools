2b7b568f50f37df404837a095b721c1f75cbf3c4
bczhc <bczhc0@126.com>
2021-07-23 11:01:03 +0800

    feat(FloatingDrawingBoard): request draw overlay permission

--- app/src/main/java/pers/zhc/tools/fdb/FdbMainActivity.kt
+++ app/src/main/java/pers/zhc/tools/fdb/FdbMainActivity.kt
@@ -1,13 +1,13 @@
 package pers.zhc.tools.fdb
 
 import android.content.Intent
-import android.content.IntentFilter
+import android.os.Build
 import android.os.Bundle
-import android.view.MotionEvent
+import android.provider.Settings
+import androidx.annotation.RequiresApi
 import kotlinx.android.synthetic.main.fdb_main_activity.*
 import pers.zhc.tools.BaseActivity
 import pers.zhc.tools.R
-import pers.zhc.tools.fdb.FdbBroadcastReceiver.Companion.ACTION_ON_SCREEN_ORIENTATION_CHANGED
 import pers.zhc.tools.utils.ToastUtils
 
 /**
@@ -15,26 +15,43 @@ import pers.zhc.tools.utils.ToastUtils
  */
 class FdbMainActivity : BaseActivity() {
 
-    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
-        return super.dispatchTouchEvent(ev)
-    }
-
-    override fun onTouchEvent(event: MotionEvent?): Boolean {
-        return super.onTouchEvent(event)
+    private val launcher = object {
+        val overlaySetting = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            registerForActivityResult(OverlaySettingContract()) {}
+        } else {
+            null
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fdb_main_activity)
 
-        val startButton = start_button!!
-        startButton.setOnClickListener {
+        val run = {
             val fdbWindow = FdbWindow(this)
             ToastUtils.show(this, fdbWindow.toString())
             fdbWindow.startFDB()
         }
 
+        val startButton = start_button!!
+        startButton.setOnClickListener {
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (!checkDrawOverlayPermission()) {
+                    launcher.overlaySetting!!.launch(this.packageName)
+                    return@setOnClickListener
+                } else run()
+            } else return@setOnClickListener
+
+            run()
+        }
+
         val serviceIntent = Intent(this, FdbService::class.java)
         startService(serviceIntent)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun checkDrawOverlayPermission(): Boolean {
+        return Settings.canDrawOverlays(this)
     }
 }
---
+++ app/src/main/java/pers/zhc/tools/fdb/OverlaySettingContract.kt
@@ -0,0 +1,22 @@
+package pers.zhc.tools.fdb
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.provider.Settings
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.annotation.RequiresApi
+
+/**
+ * @author bczhc
+ */
+@RequiresApi(Build.VERSION_CODES.M)
+class OverlaySettingContract : ActivityResultContract<String, Unit>() {
+    override fun createIntent(context: Context, input: String?): Intent {
+        return Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:$input"))
+    }
+
+    override fun parseResult(resultCode: Int, intent: Intent?) {
+    }
+}
