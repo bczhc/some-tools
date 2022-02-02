cbd608696290883ff7bdfa01e6e6b5deab8cee39
bczhc <bczhc0@126.com>
2022-02-02 17:08:29 +0800

    fix(RustBuildPlugin): forward reference in class

--- buildSrc/src/main/scala/pers/zhc/tools/plugin/rust/Toolchain.scala
+++ buildSrc/src/main/scala/pers/zhc/tools/plugin/rust/Toolchain.scala
@@ -6,10 +6,10 @@ import java.io.File
   *   bczhc
   */
 class Toolchain(val ndkPath: File, val androidApi: Int, val target: String) {
-  val linker: File = new File(binDir, s"$prefix$androidApi-clang")
-  val ar: File = new File(binDir, s"$prefix-ar")
   private val binDir = ToolchainUtils.getToolchainBinDir(ndkPath)
   private val prefix = s"$target-linux-android"
+  val linker: File = new File(binDir, s"$prefix$androidApi-clang")
+  val ar: File = new File(binDir, s"$prefix-ar")
 
   require(linker.exists())
   require(ar.exists())
