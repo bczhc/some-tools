a2aa0b96e15c4d302b6433a8aaeaf2c0a08569e3
bczhc <bczhc0@126.com>
2022-02-03 11:13:20 +0800

    fix(RustBuildPlugin): `ar` path

--- buildSrc/src/main/scala/pers/zhc/tools/plugin/rust/Toolchain.scala
+++ buildSrc/src/main/scala/pers/zhc/tools/plugin/rust/Toolchain.scala
@@ -13,9 +13,15 @@ class Toolchain(
   private val binDir = ToolchainUtils.getToolchainBinDir(ndkPath)
   private val prefix: String = androidAbi.toNdkToolchainName
 
+  val arName: String = androidAbi match {
+    case AndroidAbi.Arm    => "arm-linux-androideabi-ar"
+    case AndroidAbi.Arm64  => "aarch64-linux-android-ar"
+    case AndroidAbi.X86    => "i686-linux-android-ar"
+    case AndroidAbi.X86_64 => "x86_64-linux-android-ar"
+  }
   val linker: File =
     new File(binDir, s"$prefix$androidApi-clang")
-  val ar: File = new File(binDir, s"$prefix-ar")
+  val ar: File = new File(binDir, arName)
 
   require(linker.exists())
   require(ar.exists())
