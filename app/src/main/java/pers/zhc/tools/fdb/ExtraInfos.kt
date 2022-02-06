1750d2e7863d3053dd2e4fb78f46c1f631dd7a67
bczhc <bczhc0@126.com>
2022-02-06 16:25:51 +0800

    :bug: FloatingDrawingBoard: support old `extra_infos`

--- app/src/main/java/pers/zhc/tools/fdb/ExtraInfos.kt
+++ app/src/main/java/pers/zhc/tools/fdb/ExtraInfos.kt
@@ -1,9 +1,10 @@
 package pers.zhc.tools.fdb
 
+import android.graphics.Matrix
 import com.google.gson.Gson
-import com.google.gson.JsonSyntaxException
 import pers.zhc.jni.sqlite.SQLite3
 import pers.zhc.tools.utils.fromJsonOrNull
+import pers.zhc.tools.utils.getValuesNew
 import pers.zhc.tools.utils.withCompiledStatement
 import pers.zhc.tools.views.HSVAColorPickerRL.SavedColor
 
@@ -19,18 +20,6 @@ class ExtraInfos(
     val layersInfo: List<LayerInfo>?
 ) {
     companion object {
-        /**
-         * returns `null` if [extraInfosJson] has bad json syntax or there's
-         * no "layers" field.
-         */
-        fun getLayersInfo(extraInfosJson: String): List<LayerInfo>? {
-            return try {
-                Gson().fromJson(extraInfosJson, ExtraInfos::class.java)!!
-            } catch (e: JsonSyntaxException) {
-                return null
-            }.layersInfo
-        }
-
         private fun queryExtraInfos(db: SQLite3): String? {
             var jsonString: String? = null
             db.withCompiledStatement("SELECT extra_infos FROM info") {
@@ -50,6 +39,59 @@ class ExtraInfos(
             return Gson().fromJsonOrNull(
                 queryExtraInfos(db) ?: return null,
                 ExtraInfos::class.java
+            ) ?: (Gson().fromJsonOrNull(
+                queryExtraInfos(db) ?: return null,
+                OldExtraInfos::class.java
+            ) ?: return null).toNewExtraInfos()
+        }
+    }
+
+    @Suppress("SpellCheckingInspection")
+    private class OldMatrixData(
+        val MSCALE_X: Float,
+        val MSKEW_X: Float,
+        val MTRANS_X: Float,
+        val MSKEW_Y: Float,
+        val MSCALE_Y: Float,
+        val MTRANS_Y: Float,
+        val MPERSP_0: Float,
+        val MPERSP_1: Float,
+        val MPERSP_2: Float
+    ) {
+        fun toMatrix(): Matrix {
+            return Matrix().apply {
+                val data = floatArrayOf(
+                    MSCALE_X,
+                    MSKEW_X,
+                    MTRANS_X,
+                    MSKEW_Y,
+                    MSCALE_Y,
+                    MTRANS_Y,
+                    MPERSP_0,
+                    MPERSP_1,
+                    MPERSP_2
+                )
+                this.setValues(data)
+            }
+        }
+    }
+
+    private class OldExtraInfos(
+        val isLockingStroke: Boolean?,
+        val lockedDrawingStrokeWidth: Float?,
+        val lockedEraserStrokeWidth: Float?,
+        val savedColors: List<SavedColor>?,
+        val defaultTransformation: OldMatrixData?,
+        val layersInfo: List<LayerInfo>?
+    ) {
+        fun toNewExtraInfos(): ExtraInfos? {
+            return ExtraInfos(
+                isLockingStroke,
+                lockedDrawingStrokeWidth,
+                lockedEraserStrokeWidth,
+                savedColors,
+                (defaultTransformation ?: return null).toMatrix().getValuesNew(),
+                layersInfo
             )
         }
     }
