d1267c0129476c2c9def134331d3019823cb0a66
bczhc <bczhc0@126.com>
2022-04-07 15:36:01 +0800

    :children_crossing: Wubi: add "update inverse dict" action

--- app/src/main/java/pers/zhc/tools/wubi/WubiCodeLookingUpActivity.kt
+++ app/src/main/java/pers/zhc/tools/wubi/WubiCodeLookingUpActivity.kt
@@ -1,12 +1,15 @@
 package pers.zhc.tools.wubi
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.core.widget.doAfterTextChanged
 import kotlinx.android.synthetic.main.wubi_code_looking_up_activity.*
 import pers.zhc.tools.BaseActivity
 import pers.zhc.tools.R
 import pers.zhc.tools.utils.DialogUtils
 import pers.zhc.tools.utils.ProgressDialog
+import pers.zhc.tools.utils.ToastUtils
 
 /**
  * @author bczhc
@@ -23,22 +26,7 @@ class WubiCodeLookingUpActivity : BaseActivity() {
         if (updateNeeded) {
             DialogUtils.createConfirmationAlertDialog(this, { _, _ ->
 
-                val progressDialog = ProgressDialog(this).also {
-                    val progressView = it.getProgressView()
-                    progressView.setIsIndeterminateMode(true)
-                    progressView.setTitle(getString(R.string.updating))
-                    it.setCanceledOnTouchOutside(false)
-                    it.setCancelable(false)
-                }
-                progressDialog.show()
-
-                Thread {
-                    dict.update(DictionaryDatabase.dictDatabase.database)
-
-                    runOnUiThread {
-                        progressDialog.dismiss()
-                    }
-                }.start()
+                showUpdateInverseDict()
 
             }, titleRes = R.string.wubi_inverse_dict_update_alert).show()
         }
@@ -50,6 +38,41 @@ class WubiCodeLookingUpActivity : BaseActivity() {
             val codes = dict.query(wubiWordET.text.toString())
             resultTV.text = codes.joinToString("\n")
         }
+    }
+
+    private fun showUpdateInverseDict() {
+        val progressDialog = ProgressDialog(this).also {
+            val progressView = it.getProgressView()
+            progressView.setIsIndeterminateMode(true)
+            progressView.setTitle(getString(R.string.updating))
+            it.setCanceledOnTouchOutside(false)
+            it.setCancelable(false)
+        }
+        progressDialog.show()
+
+        Thread {
+            dict.update(DictionaryDatabase.dictDatabase.database)
+
+            runOnUiThread {
+                progressDialog.dismiss()
+                ToastUtils.show(this, R.string.process_done)
+            }
+        }.start()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.wubi_inverse_dict_activity, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.update -> {
+                showUpdateInverseDict()
+            }
+            else -> {}
+        }
+        return true
     }
 
     override fun finish() {
