e00397172efdc133929597d75f0d33fcd077e95d
bczhc <bczhc0@126.com>
2022-10-24 19:51:18 +0800

    Note app: ask whether to save on closing `NoteTakingActivity`

--- app/src/main/java/pers/zhc/tools/note/NoteTakingActivity.kt
+++ app/src/main/java/pers/zhc/tools/note/NoteTakingActivity.kt
@@ -2,22 +2,38 @@ package pers.zhc.tools.note
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import android.widget.EditText
+import androidx.core.widget.doAfterTextChanged
 import kotlinx.android.synthetic.main.take_note_activity.*
 import pers.zhc.tools.R
+import pers.zhc.tools.utils.DialogUtils
 import pers.zhc.tools.utils.ToastUtils
+import pers.zhc.tools.utils.androidAssert
 import java.io.Serializable
 
 class NoteTakingActivity : NoteBaseActivity() {
+    private lateinit var type: Type
+    private lateinit var contentET: EditText
+    private lateinit var titleET: EditText
+    private var modified = false
+
+    /**
+     * present when [type] is [Type.UPDATE]
+     */
+    private var timestamp: Long? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.take_note_activity)
 
-        val titleET = doc_title_et!!
-        val contentET = doc_content_et!!.editText
+        titleET = doc_title_et!!
+        contentET = doc_content_et!!.editText
         val bottomButton = add_record!!
 
         val intent = intent
-        val type = intent.getSerializableExtra(EXTRA_TYPE) as Type
+        type = intent.getSerializableExtra(EXTRA_TYPE) as Type
         val buttonTextRes = when (type) {
             Type.CREATE -> {
                 R.string.note_add_record_btn
@@ -31,36 +47,73 @@ class NoteTakingActivity : NoteBaseActivity() {
         when (type) {
             Type.CREATE -> {
                 bottomButton.setOnClickListener {
-                    database.addRecord(title = titleET.text.toString(), content = contentET.text.toString())
-                    ToastUtils.show(this, R.string.note_record_adding_succeeded)
+                    createNewNote()
                 }
             }
             Type.UPDATE -> {
-                val timestamp = intent.getLongExtra(EXTRA_TIMESTAMP, -1)
+                androidAssert(intent.hasExtra(EXTRA_TIMESTAMP))
+                timestamp = intent.getLongExtra(EXTRA_TIMESTAMP, -1)
                 val resultIntent = Intent().apply {
                     putExtra(EXTRA_TIMESTAMP, timestamp)
                 }
                 setResult(0, resultIntent)
 
-                val record = database.query(timestamp)!!
+                val record = database.query(timestamp!!)!!
                 titleET.setText(record.title)
                 contentET.setText(record.content)
 
                 bottomButton.setOnClickListener {
-                    database.update(
-                        timestamp, Record(
-                            timestamp, titleET.text.toString(),
-                            contentET.text.toString()
-                        )
-                    )
-                    ToastUtils.show(this, R.string.note_record_updating_succeeded)
+                    updateNote()
                 }
             }
         }
+
+        val afterTextChangedAction: (text: Editable?) -> Unit = {
+            modified = true
+        }
+        titleET.doAfterTextChanged(afterTextChangedAction)
+        contentET.doAfterTextChanged(afterTextChangedAction)
     }
 
     enum class Type : Serializable {
         CREATE, UPDATE
+    }
+
+    private fun createNewNote() {
+        database.addRecord(title = titleET.text.toString(), content = contentET.text.toString())
+        ToastUtils.show(this, R.string.note_record_adding_succeeded)
+    }
+
+    private fun updateNote() {
+        val timestamp = this.timestamp!!
+        database.update(
+            timestamp, Record(
+                timestamp, titleET.text.toString(),
+                contentET.text.toString()
+            )
+        )
+        ToastUtils.show(this, R.string.note_record_updating_succeeded)
+    }
+
+    override fun finish() {
+        if (modified) {
+            DialogUtils.createConfirmationAlertDialog(this, titleRes = R.string.note_modified_save_alert,
+                width = MATCH_PARENT,
+                positiveAction = { _, _ ->
+                    when (type) {
+                        Type.CREATE -> {
+                            createNewNote()
+                        }
+                        Type.UPDATE -> {
+                            updateNote()
+                        }
+                    }
+                    super.finish()
+                }
+            ).show()
+        } else {
+            super.finish()
+        }
     }
 
     companion object {
