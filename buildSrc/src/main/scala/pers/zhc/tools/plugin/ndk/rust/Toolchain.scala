3bb5280ef030c1bdac12e3d3ad6931c62aee68f3
bczhc <bczhc0@126.com>
2022-11-10 09:52:50 +0800

    use new 'llvm-ar' in Android NDK
    GNU ar was deprecated in r22 and removed in r23.
    
    - https://github.com/android/ndk/wiki/Changelog-r22
    - https://github.com/android/ndk/wiki/Changelog-r23

--- buildSrc/src/main/scala/pers/zhc/tools/plugin/ndk/rust/Toolchain.scala
+++ buildSrc/src/main/scala/pers/zhc/tools/plugin/ndk/rust/Toolchain.scala
@@ -14,12 +14,7 @@ class Toolchain(
   private val binDir = ToolchainUtils.getToolchainBinDir(ndkPath)
   private val prefix: String = androidAbi.toNdkToolchainName
 
-  val arName: String = androidAbi match {
-    case AndroidAbi.Arm    => "arm-linux-androideabi-ar"
-    case AndroidAbi.Arm64  => "aarch64-linux-android-ar"
-    case AndroidAbi.X86    => "i686-linux-android-ar"
-    case AndroidAbi.X86_64 => "x86_64-linux-android-ar"
-  }
+  val arName: String = "llvm-ar"
   val linker: File =
     new File(binDir, s"$prefix$androidApi-clang")
   val ar: File = new File(binDir, arName)
