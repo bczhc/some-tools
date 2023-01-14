43baca011f7854e187991d644f126f87de8f1e3e
bczhc <bczhc0@126.com>
2023-01-14 21:54:47 +0800

    upgrade to Kotlin 1.8.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ apply {
 plugins {
     id("com.android.application") version "7.3.0" apply false
     id("com.android.library") version "7.3.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.7.21" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.0" apply false
 }
 
 task("clean", type = Delete::class) {
