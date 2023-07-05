1dde5bc48c64afebbe52d7cc909221a6a104db56
Zhai Can <bczhc0@126.com>
2023-07-05 12:58:33 +0800

    fix weird indent

--- app/src/main/java/pers/zhc/tools/utils/AllFilesAccessPermissionRequestContract.kt
+++ app/src/main/java/pers/zhc/tools/utils/AllFilesAccessPermissionRequestContract.kt
@@ -4,21 +4,20 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Build
-import android.os.Environment
 import android.provider.Settings
 import androidx.activity.result.contract.ActivityResultContract
 import androidx.annotation.RequiresApi
 import pers.zhc.tools.BuildConfig
 
 @RequiresApi(Build.VERSION_CODES.R)
-    class AllFilesAccessPermissionRequestContract : ActivityResultContract<Unit, Unit>() {
-        override fun createIntent(context: Context, input: Unit): Intent {
-            return Intent(
-                Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION,
-                Uri.parse("package:${BuildConfig.APPLICATION_ID}")
-            )
-        }
-
-        override fun parseResult(resultCode: Int, intent: Intent?) {
-        }
+class AllFilesAccessPermissionRequestContract : ActivityResultContract<Unit, Unit>() {
+    override fun createIntent(context: Context, input: Unit): Intent {
+        return Intent(
+            Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION,
+            Uri.parse("package:${BuildConfig.APPLICATION_ID}")
+        )
     }
+
+    override fun parseResult(resultCode: Int, intent: Intent?) {
+    }
+}
