0f2ef7d43de0241298b32ffab72abdd3ba252f61
undefined303 <undefined303@qq.com>
2024-02-03 22:55:12 +0800

    tts改为多行，增加最大字符限制为4000（最大朗读字数）

--- app/src/main/java/pers/zhc/tools/test/TTS.java
+++ app/src/main/java/pers/zhc/tools/test/TTS.java
@@ -3,10 +3,10 @@ package pers.zhc.tools.test;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
 import android.widget.Button;
-import android.widget.EditText;
 import androidx.annotation.Nullable;
 import pers.zhc.tools.BaseActivity;
 import pers.zhc.tools.R;
+import pers.zhc.tools.views.ScrollEditText;
 
 public class TTS extends BaseActivity {
     private TextToSpeech tts;
@@ -16,7 +16,7 @@ public class TTS extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tts_test_activity);
         tts = new TextToSpeech(this, null);
-        EditText et = findViewById(R.id.et);
+        ScrollEditText et = findViewById(R.id.et);
         Button button = findViewById(R.id.btn);
         button.setOnClickListener(v -> tts.speak(et.getText().toString(), TextToSpeech.QUEUE_ADD, null, String.valueOf(System.currentTimeMillis())));
     }
